37445e96ece9bb84ea09ff3f29f2bd607cd505b1
Nate Geslin <teamtomkins23@gmail.com>
2015-07-16 00:57:59 -0500

    Adds FastMath class, a future home for all the math calculations that will be needed.

--- public/assets/scripts/lib/util/FastMath.js
+++ public/assets/scripts/lib/util/FastMath.js
@@ -1,1 +1,26 @@
-// FastMath
+define([], function() {
+    'use strict';
+
+    /**
+     * @class  FastMath
+     * #constructor
+     */
+    function FastMath() {}
+
+    /**
+     * @method  abs
+     * @for  FastMath
+     * @param  a {number}
+     * @return {number} absoulte value of a
+     */
+    FastMath.prototype.abs = function abs(a) {
+        if (a < 0) {
+            return -a;
+        }
+
+        return a;
+    };
+
+
+    return FastMath;
+});
